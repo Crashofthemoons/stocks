e3d8f6e086b635dd66417e3e952f56adacf39c98
Jennifer Lawson <jenniferlawson000@gmail.com>
2018-06-12 11:10:18 -0500

    first commit

---
+++ stocks.js
@@ -0,0 +1,70 @@
+const stocks = Object.create({}, {
+    company: {
+        value: 'Star Fleet',
+        enumerable: true,
+        writable: true
+    },
+    specialty: {
+        value: 'technology',
+        enumerable: true,
+        writable: true
+    },
+    name: {
+        value: 'Data',
+        enumerable: true
+    },
+    portfolio: {
+        value: [
+            {name: "AAPL",
+            shares: 152,
+            valuation: 30400,
+            purchased: true},
+    ],
+    },
+    worth: {
+        value: function () {
+            let netWorth = this.portfolio
+            let dollarAmount = 0
+            netWorth.forEach(object => {
+                dollarAmount += object.valuation
+            })
+            return dollarAmount
+        }
+    },
+    transaction: {
+        value: function (symbol, quantity, price, boo) {
+           this.portfolio.push({name: symbol,
+            shares: quantity,
+            valuation: price,
+            purchased: boo})
+        }
+    }
+})
+stocks.transaction("HELL", 666, 9000, true)
+stocks.transaction('MSFT', 290, 49300, false)
+
+console.log(stocks.worth())
+console.log(stocks)
+
+const fragment = document.createDocumentFragment()
+const articles = document.querySelector('#dude')
+
+for (let key in stocks) { 
+    
+    const section = document.createElement('section')
+    section.textContent = `${key.charAt(0).toLocaleUpperCase() + key.slice(1)}: ${stocks[key]}`
+    
+    fragment.appendChild(section)
+}
+
+let port = stocks.portfolio
+port.forEach(object => {
+    for (key in object) {
+        const portSection = document.createElement('section')
+        portSection.textContent = `${key.charAt(0).toLocaleUpperCase() + key.slice(1)}: ${object[key]}`
+        fragment.appendChild(portSection)
+    }
+})
+
+
+articles.appendChild(fragment)
